829172fdaf4b4165550573a3deb1b1bf74bf226a
Amit Parakar <71601718+Amit2468Shukla@users.noreply.github.com>
2025-09-13 04:07:03 +0000

    definition.js

---
+++ 01_basics/definition.js
@@ -0,0 +1,9 @@
+const accountId="4682134"
+let accountEmail="abc@gmail.com"
+var password="24680"
+accountCity="Hyderabad"
+
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,password,accountCity,accountState])
