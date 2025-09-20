23c11885cec1dd1dc40b51d1ee34857b938cd828
Amit Parakar <71601718+Amit2468Shukla@users.noreply.github.com>
2025-09-20 08:12:23 +0000

    operation.js

---
+++ 01_basics/operations.js
@@ -0,0 +1,37 @@
+console.log(2**4);
+let value = 2;
+let negativeValue = -value;
+console.log(negativeValue);
+
+// let str1 = "Hello"
+// let str2 = " Amit"
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log(2 + "4");
+// console.log(2 + "4" + 5 + 6);
+// console.log("4" + 2 + 6 + 8);
+// console.log("4" + "8");
+// console.log("4" + "8"  + 6 + 2);
+// console.log(2 + 4 + "8");
+
+// console.log(+true);//1
+// console.log(+"");//0
+// console.log("");//empty string
+// console.log(" ");//empty string
+// console.log(+false);//0
+// console.log(1);//1
+// console.log(0);//0
+// console.log(+null);//0
+// console.log(+undefined);//NaN
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+console.log(++gameCounter);
+console.log(gameCounter++);//102  + gameCounter++);
+console.log(gameCounter++ + gameCounter++);//207
+console.log(gameCounter++ + gameCounter++ + gameCounter++);
+// console.log(gameCounter);
+// console.log(--gameCounter);
+// console.log(gameCounter--);
+// console.log(gameCounter);
